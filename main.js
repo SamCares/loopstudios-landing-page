ef2197e5b03bf3adc2efcd9e4656b01a679baeb9
SamCares <44313759+SamCares@users.noreply.github.com>
2022-08-15 23:24:51 +0100

    new

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-document.getElementById("btn").onclick = function () { seeAll() };
+// document.getElementById("btn").onclick = function () { seeAll() };
 
-function seeAll() {
-    document.querySelectorAll("#see-all").classList.toggle("show");
+// function seeAll() {
+//     document.querySelectorAll("see-all").classList.toggle("show");
+// }
+
+
+const button = document.querySelector('btn');
+const seeAll = document.querySelectorAll('see-all');
+
+button.onMouseclick = function () {
+    seeAll.classList.toggle('show');
 }
