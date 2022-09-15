9e17747cb455a062989ecc78d09cd1bc7dbb9b50
SamCares <44313759+SamCares@users.noreply.github.com>
2022-09-15 16:39:33 +0100

    hamburger toggle

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const seeAllButton = document.querySelector('.btn');
 const collapseButton = document.querySelector('.btn2')
 const seeAll = document.querySelectorAll('.see-all');
+const hamburger = document.querySelector('.hamburger');
+const close = document.querySelector('.close');
 
 seeAllButton.addEventListener('click', function () {
     seeAll.forEach(seeAll => {
@@ -17,3 +19,14 @@ collapseButton.addEventListener('click', function () {
         btn2.style.display = "none"
     })
 });
+
+hamburger.addEventListener('click', function () {
+    close.style.display = "block";
+    hamburger.style.display = "none";
+})
+
+close.addEventListener('click', function () {
+    close.style.display = "none";
+    hamburger.style.display = "block";
+})
+
